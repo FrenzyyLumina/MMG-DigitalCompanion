a8c82e0ca4ead3cc64ff8caee199a586669efb28
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-12-05 03:41:38 +0800

    Added Room core class

---
+++ MMG Digital Companion/Assets/Scripts/Room.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    private int[] coordinate;
+    private GameEnums.SquareType type;
+    private bool isCompleted;
+
+    public Room(int x, int y, GameEnums.SquareType type)
+    {
+        coordinate = new int[2] { x, y };
+        isCompleted = false;
+        this.type = type;
+    }
+
+    public bool hasCoordinate(int x, int y)
+    {
+        return coordinate[0] == x && coordinate[1] == y;
+    }
+
+    public bool getIsCompleted()
+    {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(bool val)
+    {
+        isCompleted = val;
+    }
+
+    public GameEnums.SquareType getType()
+    {
+        return type;
+    }
+    public void setType(GameEnums.SquareType type)
+    {
+        this.type = type;
+    }
+}
