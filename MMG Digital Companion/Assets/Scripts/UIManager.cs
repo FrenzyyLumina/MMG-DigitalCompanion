4172f6c38da79abe0fb2fddddaf31b37e085b20a
Frenzyy <72508034+FrenzyyLumina@users.noreply.github.com>
2025-12-05 05:30:54 +0800

    Update UIManager.cs

--- MMG Digital Companion/Assets/Scripts/UIManager.cs
+++ MMG Digital Companion/Assets/Scripts/UIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     private readonly string MAIN_MENU_SCENE = "Main Menu";
+    private readonly string GAME_START_SCENE = "GameStart";
     private readonly string GAME_SCENE = "Game";
     private readonly string QR_SCANNER_SCENE = "QRScanner";
 
@@ -14,6 +15,25 @@ public class UIManager : MonoBehaviour
         print("Application Quit");
         Application.Quit();
     }
+
+    public void OnGameStart()
+    {
+        // Check if player count has been set, if not default to 2
+        if (GameManager.Instance != null)
+        {
+            if (GameManager.Instance.TotalPlayers == 0)
+            {
+                GameManager.Instance.SetPlayerCount(2);
+            }
+            GameManager.Instance.StartGameStartScene();
+        }
+        else
+        {
+            // Fallback if GameManager doesn't exist
+            SceneManager.LoadScene(GAME_START_SCENE);
+        }
+    }
+
     public void OnGameStarted()
     {
         SceneManager.LoadScene(GAME_SCENE);
