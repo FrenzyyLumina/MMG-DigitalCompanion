40b19a12320ac5255cd9b5ac56ca2340869d7922
mjbee <90549979+17mjbee@users.noreply.github.com>
2025-12-04 19:04:45 +0800

    qr scanner script
    might need another version of zxing for the error

--- MMG Digital Companion/Assets/Scripts/QRScanner.cs
+++ MMG Digital Companion/Assets/Scripts/QRScanner.cs
@@ -5,17 +5,93 @@ using ZXing;
 using TMPro;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class QRScanner : MonoBehaviour
 {
+
+    [SerializeField]
+    private RawImage _rawImageBG;
+    [SerializeField]
+    private AspectRatioFitter _aspectRatioFitter;
+    [SerializeField]
+    private TextMeshProUGUI _textResult;
+    [SerializeField]
+    private RectTransform _scanZone;
+
+    private bool _isCamAvailable;
+    private WebCamTexture _camTexture;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpCamera();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateCameraRender();
+    }
+
+    private void SetUpCamera(){
         
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if(devices.Length == 0)
+        {
+            _isCamAvailable = false;
+            return;
+        }
+        for(int i = 0; i<devices.Length; i++)
+        {
+            if(devices[i].isFrontFacing == false)
+            {
+                _camTexture = new WebCamTexture(devices[i].name, (int)_scanZone.rect.width, (int)_scanZone.rect.height);
+            }
+        }
+        _camTexture.Play();
+        _rawImageBG.texture = _camTexture;
+        _isCamAvailable = true;
+    }
+
+    private void UpdateCameraRender() 
+    { 
+        if (_isCamAvailable == false)
+        {
+            return;
+        }
+        float ratio = (float)_camTexture.width / (float)_camTexture.height;
+        _aspectRatioFitter.aspectRatio = ratio;
+
+        int orientation = _camTexture.videoRotationAngle;
+        _rawImageBG.rectTransform.localEulerAngles = new Vector3(0,0, orientation);
+    }
+
+    public void OnClickScan()
+    {
+        if(_isCamAvailable)
+        {
+            Scan();
+        }
+    }
+
+    private void Scan()
+    {
+        try
+        {
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            Result result = barcodeReader.Decode(_camTexture.GetPixels32(), _camTexture.width, _camTexture.height);
+            if (result != null)
+            {
+                _textResult.text = result.Text;
+            }
+            else
+            {
+                _textResult.text = "FAILED TO READ CODE!";
+            }
+        }
+        catch
+        {
+            _textResult.text = "FAIL!";
+        }
     }
 }
