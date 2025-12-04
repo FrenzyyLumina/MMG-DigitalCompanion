01943e1cfefce38048e1aa5be47015cbdb6b0923
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-12-04 16:14:45 +0800

    Modified how add/remove item works

--- MMG Digital Companion/Assets/Scripts/Inventory.cs
+++ MMG Digital Companion/Assets/Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
@@ -14,12 +15,45 @@ public class Inventory : MonoBehaviour
     {
         return this.inventory;
     }
+    public Item findItemByName(string name)
+    {
+        foreach (Item item in inventory)
+        {
+            if (item.name == name)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
     public void addItem(Item item)
     {
-        this.inventory.Add(item);
+        Item exist = this.findItemByName(item.name);
+        if (exist != null)
+        {
+            exist.incCount();
+        }
+        else
+        {
+            this.inventory.Add(item);
+        }
+    }
+    public void removeItemByName(string name)
+    {
+        Item exist = this.findItemByName(name);
+        if (exist == null)
+        {
+            print("No Item Found");
+            return;
+        }
+
+        exist.decCount();
+        if (exist.getCount() == 0)
+            this.inventory.Remove(exist);
     }
     public void removeItem(Item item)
     {
-        this.inventory.Remove(item);
+        this.removeItemByName(item.name);
     }
 }
