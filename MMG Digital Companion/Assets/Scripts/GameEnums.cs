d8c79416706e51961b1db7328a18a3232faaf9f4
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2025-12-05 13:26:01 +0800

    Added complete item enum

--- MMG Digital Companion/Assets/Scripts/GameEnums.cs
+++ MMG Digital Companion/Assets/Scripts/GameEnums.cs
@@ -66,6 +66,32 @@ public static class GameEnums
     public enum Item
     {
         None,
+
+        //Mechanic Related
         Dice,
+        McGuffin,
+
+        //Defensive
+        Body_Armor,
+        Personal_Radar,
+        Rations,
+        Adrenaline,
+        Active_Projectile_Dome,
+        Revival_Pill,
+
+        //Offensive
+        Hush_Puppy,
+        Poison_Blowdart,
+        M9_Bayonet,
+        Tripmine,
+
+        //Utility
+        Universal_Multi_Tool,
+        Proximity_Detector,
+        Truth_Serum,
+        Sneaking_Suit,
+        Cardboard_Box,
+        Coin,
+        The_Doohickey,
     }
 }
